a74c320755af118219579c350e905bdc848d518e
denosaurabh <denosaurabh@gmail.com>
2023-05-20 16:44:37 +0530

    hide `Save Objects` button if there's no draft objects

--- packages/client/src/ui/footer.tsx
+++ packages/client/src/ui/footer.tsx
@@ -19,6 +19,7 @@ export const Footer = () => {
       <div className="rounded-lg bg-slate-700 flex justify-end gap-2 p-1.5 bg-opacity-70">
         <button
           onClick={onSaveObjectsClick}
+          style={{ display: draftObjects.length ? "flex" : "none" }}
           className="uppercase w-fit px-5 py-2 text-lg text-emerald-200 bg-emerald-600 rounded-md
         cursor-pointer transition-all hover:bg-emerald-500 hover:rotate-3 hover:shadow-xl active:shadow-sm active:rotate-1 active:bg-emerald-600 active:translate-y-0.5 focus:outline focus:outline-4 focus:outline-offset-2 focus:outline-emerald-200"
         >
